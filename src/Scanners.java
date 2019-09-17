0dec96987813be8d25f8f268eb03ded266651369
Carditis <45385019+Carditis@users.noreply.github.com>
2019-09-17 08:19:20 +0200

    Fuck pyramider, made scanners work
    I really hate pyramider, men har fået scanners til at virke. I know that this is bad commit

--- src/Scanners.java
+++ src/Scanners.java
@@ -1,4 +1,19 @@
+import  java.util.Scanner;
+
 
 public class Scanners {
+	public static void main(String[] args) {
+		  Scanner calcu = new Scanner(System.in);
+		    System.out.println("Skriv nogle tal");
 
+		    int talEt = calcu.nextInt();
+		    int talTo = calcu.nextInt();
+		    int n = talEt+talTo;
+		    System.out.println(n);  
+		
+	}
+	
+	
+	
+	
 }
--- src/pyramide.java
+++ src/pyramide.java
@@ -2,22 +2,25 @@
 public class pyramide {
 
 	public static void main(String[] args) {
+		int bredde = 11;
+		for(int i= 1; i<10; i=i+2) {
+			a
+			for(int j= 0 ; j<antal)
+			
+			
+		for(int j=0; j<i; j++)
+			System.out.print(i);
 		
-		for(int linie=0; linie<5; linie++) {
-			
-		for(int i=0; i<5-linie; i++)
-			System.out.print("-");
-			
-		int antal = 2*linie+1;
-		for(int i = 0; i<antal; i++)
-			System.out.print(antal);
-		skrivminus(5-linie);
 		
 		System.out.println();
 		}
-		public static skrivminus(int antalminus) {
-			for(int i = 0; i<antalminus; i++)
-				System.out.print("-");
+		
+		
+		
+		
+		
+		
+	
 			
 		}
 	
