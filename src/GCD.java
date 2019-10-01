64866832208c44cf219dec40b436fe562ababd33
Carditis <45385019+Carditis@users.noreply.github.com>
2019-10-01 23:13:58 +0200

    fuck you
    cdl

--- src/GCD.java
+++ src/GCD.java
@@ -2,7 +2,7 @@
 public class GCD {
 	public static void main(String[] args) {
 
-		System.out.print(gcd(497,322));
+		System.out.print(gcd(-36,60));
 	}
 	public static int gcd(int a, int b) {
 		int h;
@@ -11,7 +11,8 @@ public class GCD {
 			b = a % b;
 			a = h;
 		}
-
+		if(a<0)
+			a = a * -1;
 		return a;
 
 
